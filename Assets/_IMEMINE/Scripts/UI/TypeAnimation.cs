a2a2d7a68fc83be457fdb3872266559b632e19f1
Menno Buggenhout <mennobuggenhout@gmail.com>
2024-04-14 19:20:38 +0200

    Fonts and typing sounds

--- Assets/_IMEMINE/Scripts/UI/TypeAnimation.cs
+++ Assets/_IMEMINE/Scripts/UI/TypeAnimation.cs
@@ -6,6 +6,7 @@ using Sirenix.OdinInspector;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TypeAnimation : MonoBehaviour
 {
@@ -14,18 +15,21 @@ public class TypeAnimation : MonoBehaviour
     [SerializeField] private float characterTypeSpeed;
     [SerializeField] private float spaceWaitTime;
     [SerializeField] private float periodWaitTime;
+    [SerializeField] private float percentageOfTypingSounds;
+    [SerializeField] private AudioClip[] typingClips;
+    [SerializeField] private AudioSource typingSoundSource;
 
     private Coroutine typingRoutine;
 
     [Button]
     public void TypeAnimate(string text)
     {
-        if(typingRoutine != null)
+        if (typingRoutine != null)
             StopCoroutine(typingRoutine);
 
         typingRoutine = StartCoroutine(DoTyping(text));
     }
-    
+
     private IEnumerator DoTyping(string text)
     {
         for (int i = 0; i < text.Length; i++)
@@ -44,13 +48,34 @@ public class TypeAnimation : MonoBehaviour
             {
                 secondsToWait = spaceWaitTime;
             }
-            
+            else
+            {
+                PlayNextRandomTypingSound();
+            }
+
             if (i == text.Length - 1)
                 isShowingCursor = false;
-            
+
             textComponent.text = $"{partToShow}{(isShowingCursor ? cursorCharacter : "")}";
-            
+
             yield return new WaitForSeconds(secondsToWait);
         }
+    }
+
+    private void PlayNextRandomTypingSound()
+    {
+        if(Random.value < percentageOfTypingSounds)
+            return;
+        
+        int randIndex = Random.Range(0, typingClips.Length);
+        AudioClip clip = typingClips[randIndex];
+
+        typingSoundSource.Stop();
+        
+        typingSoundSource.clip = clip;
+        float randPitch = 0.5f + Random.value;
+        typingSoundSource.pitch = randPitch;
+        
+        typingSoundSource.Play();
     }
 }
